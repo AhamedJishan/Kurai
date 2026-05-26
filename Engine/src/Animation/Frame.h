231faf6223af2b5d68978620ccb87b51ff7ee539
Jishan Ahamed <146475810+AhamedJishan@users.noreply.github.com>
2026-05-26 19:17:19 +0530

    Frame class added

---
+++ Engine/src/Animation/Frame.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Dawn
+{
+	template <unsigned int N>
+	class Frame
+	{
+	public:
+		float mValue[N];
+		float mInTanget;
+		float mOutTangent;
+		float mTime;
+	};
+
+	typedef Frame<1> ScalarFrame;
+	typedef Frame<2> Vec2Frame;
+	typedef Frame<3> Vec3Frame;
+	typedef Frame<4> QuaternionFrame;
+}
