503f6da8a11dfed9b0034aad780fa9886dbd18a1
Jishan Ahamed <146475810+AhamedJishan@users.noreply.github.com>
2026-07-15 19:23:01 +0530

    Rename VertexBoneData -> VertexSkinData, added helper method to fill weights

--- Engine/src/Asset/Vertex.h
+++ Engine/src/Asset/Vertex.h
@@ -15,10 +15,30 @@ namespace Dawn
 		glm::vec3 Tangent;
 	};
 
-	struct VertexBoneData
+	struct VertexSkinData
 	{
 	public:
-		glm::ivec4 BoneIds;
-		glm::vec4 BoneWeights;
+		void AddWeight(int boneId, float weight)
+		{
+			if (Weights[0] == 0.0f)
+			{
+				Ids[0] = boneId;
+				Weights[0] = weight;
+				return;
+			}
+
+			int smallestWeightIndex = 0;
+
+			for (int i = 0; i < 4; i++)
+				if (Weights[i] < Weights[smallestWeightIndex])
+					smallestWeightIndex = i;
+
+			Ids[smallestWeightIndex] = boneId;
+			Weights[smallestWeightIndex] = weight;
+		}
+
+	public:
+		glm::ivec4 Ids = glm::ivec4(0);
+		glm::vec4 Weights = glm::vec4(0.0f);
 	};
 }
