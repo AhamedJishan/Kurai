38194ecaed73a6e47c0f81dc2fc028319e9bd362
Jishan Ahamed <146475810+AhamedJishan@users.noreply.github.com>
2026-08-21 18:45:21 +0530

    Inspector can now show components

--- DawnEditor/src/DawnEditor/Inspector.cpp
+++ DawnEditor/src/DawnEditor/Inspector.cpp
@@ -1,11 +1,49 @@
 #include "Inspector.h"
 
 #include <imgui/imgui.h>
+#include <imgui/imgui_stdlib.h>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <Dawn/Core/Application.h>
+#include <Dawn/Core/ComponentFactory.h>
 #include <Dawn/Core/Actor.h>
+#include <Dawn/Core/Component.h>
+#include <Dawn/Core/Property.h>
 #include <Dawn/Core/Transform.h>
 
 namespace Dawn::Editor
 {
+	bool DrawQuat(const char* label, glm::quat& value, float speed = 0.1f, float min = 0.0f, float max = 0.0f, const char* format = "%.2f")
+	{
+		ImGuiStorage* imGuiStorage = ImGui::GetStateStorage();
+		ImGuiID idx = ImGui::GetID((std::string(label) + "_x").c_str());
+		ImGuiID idy = ImGui::GetID((std::string(label) + "_y").c_str());
+		ImGuiID idz = ImGui::GetID((std::string(label) + "_z").c_str());
+
+		glm::vec3 rotEuler;
+		rotEuler.x = imGuiStorage->GetFloat(idx);
+		rotEuler.y = imGuiStorage->GetFloat(idy);
+		rotEuler.z = imGuiStorage->GetFloat(idz);
+
+		glm::quat reconstructed = glm::quat(glm::radians(rotEuler));
+		if (std::abs(glm::dot(reconstructed, value)) < 0.9999f)
+			rotEuler = glm::degrees(glm::eulerAngles(value));
+
+		bool edited = ImGui::DragFloat3("##Rotation", &rotEuler[0], speed, min, max, format);
+
+		if (edited)
+			value = glm::quat(glm::radians(rotEuler));
+
+		imGuiStorage->SetFloat(idx, rotEuler.x);
+		imGuiStorage->SetFloat(idy, rotEuler.y);
+		imGuiStorage->SetFloat(idz, rotEuler.z);
+
+		return edited;
+	}
+
 	void DrawTransform(Actor* actor)
 	{
 		if (ImGui::CollapsingHeader("Transform", ImGuiTreeNodeFlags_DefaultOpen))
@@ -31,16 +69,70 @@ namespace Dawn::Editor
 				ImGui::SetNextItemWidth(-1);
 				ImGui::DragFloat3("##Scale", &transform.Scale[0], .10f, 0.0f, 0.0f, "%.2f");
 
-				glm::vec3 rotEuler = glm::degrees(glm::eulerAngles(transform.Rotation));
-
 				ImGui::TableNextRow();
 				ImGui::TableNextColumn();
 				ImGui::Text("Rotation");
 				ImGui::TableNextColumn();
 				ImGui::SetNextItemWidth(-1);
-				if (ImGui::DragFloat3("##Rotation", &rotEuler[0], .10f, 0.0f, 0.0f, "%.2f"))
-					transform.Rotation = glm::quat(glm::radians(rotEuler));
+				DrawQuat("##Rotation", transform.Rotation);
 					
+				ImGui::EndTable();
+			}
+		}
+	}
+
+	// returns bool, whether the property was changed or not
+	bool DrawProperty(Property property)
+	{
+		std::string propertyLabel("##" + property.name);
+
+		ImGui::TableNextColumn(); 
+		ImGui::Text(property.name.c_str());
+
+		ImGui::TableNextColumn();
+		ImGui::SetNextItemWidth(-1);
+
+		bool edited = false;
+
+		switch (property.type)
+		{
+		case PropertyType::Int: edited = ImGui::DragInt(propertyLabel.c_str(), static_cast<int*>(property.data), 0.1f); break;
+		case PropertyType::Bool: edited = ImGui::Checkbox(propertyLabel.c_str(), static_cast<bool*>(property.data)); break;
+		case PropertyType::Float: edited = ImGui::DragFloat(propertyLabel.c_str(), static_cast<float*>(property.data), 0.1f); break;
+		case PropertyType::Vec2: edited = ImGui::DragFloat2(propertyLabel.c_str(), glm::value_ptr(*static_cast<glm::vec2*>(property.data)), 0.1f); break;
+		case PropertyType::Vec3: edited = ImGui::DragFloat3(propertyLabel.c_str(), glm::value_ptr(*static_cast<glm::vec3*>(property.data)), 0.1f); break;
+		case PropertyType::Vec4: edited = ImGui::DragFloat4(propertyLabel.c_str(), glm::value_ptr(*static_cast<glm::vec4*>(property.data)), 0.1f); break;
+		case PropertyType::Quat: edited = DrawQuat(propertyLabel.c_str(), *static_cast<glm::quat*>(property.data)); break;
+		case PropertyType::String: 
+			ImGui::InputText(propertyLabel.c_str(), static_cast<std::string*>(property.data), ImGuiInputTextFlags_EnterReturnsTrue);
+			edited = ImGui::IsItemDeactivatedAfterEdit();
+			break;
+		// TODO: StringList and StrinPairList
+		default: break;
+		}
+
+		return edited;			
+	}
+
+	void DrawComponent(Component* component)
+	{
+		std::string componentName = Application::Get()->GetComponentFactory()->GetComponentName(component);
+		std::vector<Property> properties = component->GetProperties();
+
+		if (ImGui::CollapsingHeader(componentName.c_str(), ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			if (ImGui::BeginTable(componentName.c_str(), 2))
+			{
+				float availableWidth = ImGui::GetContentRegionAvail().x;
+				ImGui::TableSetupColumn("Property", ImGuiTableColumnFlags_WidthFixed, 0.3f * availableWidth);
+				ImGui::TableSetupColumn("Value", ImGuiTableColumnFlags_WidthStretch);
+
+				for (Property& property : properties)
+				{
+					ImGui::TableNextRow();
+					if (DrawProperty(property))
+						component->OnPropertiesChanged();
+				}
 				ImGui::EndTable();
 			}
 		}
@@ -55,10 +147,10 @@ namespace Dawn::Editor
 		if (!selectedActor)
 			return;
 
-		// Display Transform
 		DrawTransform(selectedActor);
 
-		// Display each property
+		for (Component* component : selectedActor->GetComponents())
+			DrawComponent(component);
 
 		// Add Component
 	}
